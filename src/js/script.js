5d98026df52ef0ca019a840f1d509ccd93ffdc4c
Максим Сергеевич <krovorgen@gmail.com>
2021-02-07 22:39:20 +0300

    optimized pictures

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,11 @@
 }; */
 
 $(function () {
+    $("a[href^='#']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
     $('.products__slider').slick({
         prevArrow: '<button aria-label="prev" class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
         nextArrow: '<button aria-label="prev" class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
@@ -33,11 +38,11 @@ $(function () {
         $(this).toggleClass('in').next().slideToggle();
         $('.questions__item-title').not(this).removeClass('in').next().slideUp();
     });
-    $('.menu__btn').on('click', function() {
+    $('.menu__btn').on('click', function () {
         $('.menu__btn').toggleClass('menu__btn--active');
         $('.menu__list').toggleClass('menu__list--active');
     });
-    $('.menu__list-link').on('click', function() {
+    $('.menu__list-link').on('click', function () {
         $('.menu__btn').removeClass('menu__btn--active');
         $('.menu__list').removeClass('menu__list--active');
     });
